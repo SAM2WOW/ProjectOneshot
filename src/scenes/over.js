7ed6a0fe62ef3b4373fbfdd73aa9d8c1a17ed1b9
Ryoma Marta-Sugawara <ryomams@users.noreply.github.com>
2022-05-01 12:01:49 -0700

    added new assets

--- src/scenes/over.js
+++ src/scenes/over.js
@@ -4,18 +4,26 @@ class Over extends Phaser.Scene {
     }
 
     preload() {
-
-        
-
+        this.load.spritesheet('game_over', 'assets/sprites/deadspritesheet.png', { frameWidth: 320, frameHeight: 250 });
     }
 
     create() {
-        this.title = this.add.text(game.config.width / 2, game.config.height / 2, ["GAME", "OVER"], {fontFamily: "PixelFont"});
-        this.title.setOrigin(0.5, 0.5);
-        this.title.setFontSize(64);
+        //this.title = this.add.text(game.config.width / 2, game.config.height / 2, ["GAME", "OVER"], {fontFamily: "PixelFont"});
+        //this.title.setOrigin(0.5, 0.5);
+        //this.title.setFontSize(64);
 
         this.input.on("pointerdown", () => {
             this.scene.start("menu");
         });
+
+        this.anims.create({
+            key: 'game_over',
+            frames: this.anims.generateFrameNumbers('game_over', { start: 0, end: 1, first: 0}),
+            frameRate: 5,
+            repeat: -1
+        });
+
+        this.game_over = this.add.sprite(game.config.width / 2, game.config.height, 'game_over').setOrigin(0.5, 1)
+        this.game_over.anims.play('game_over');
     }
 }

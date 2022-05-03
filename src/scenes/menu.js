05bd7cab7754afaa7200a60104a230ec1e660bec
Ryoma Marta-Sugawara <ryomams@users.noreply.github.com>
2022-05-03 02:07:53 -0700

    logo now appears on title screen

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -4,19 +4,24 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-        this.load.spritesheet('tutorial', 'assets/Tutorial.png', { frameWidth: 360, frameHeight: 360 });
+        //this.load.spritesheet('tutorial', 'assets/Tutorial.png', { frameWidth: 360, frameHeight: 360 });
+        this.load.image('title', 'assets/sprites/spookylogo.png');
     }
 
     create() {       
-        this.title = this.add.text(game.config.width / 2, 0 + 30, ["SPOOKY", "PICTURE", "GAME"]);
-        this.title.setOrigin(0.5, 0);
-        this.title.setFontSize(64);
+        this.title = this.add.text(10, game.config.height - game.config.height / 10, ["Sam Feng", "Jose Salas", "Ryoma Marta-Sugawara", "UCSC CMPM 120-01 Spring '22"]);
+        this.title.setOrigin(0, 0);
+        this.title.setFontSize(8);
 
         this.input.on("pointerdown", () => {
             this.scene.start("play");
         });
 
-        this.anims.create({
+        this.titleSprite = this.add.image(game.config.width /2,  game.config.height / 8, 'title').setOrigin(0.5,0);
+        this.titleSprite.setScale(1 * (game.config.width/game.config.height));
+
+
+        /*this.anims.create({
             key: 'tutorial',
             frames: this.anims.generateFrameNumbers('tutorial', { start: 0, end: 1, first: 0}),
             frameRate: 1,
@@ -24,6 +29,6 @@ class Menu extends Phaser.Scene {
         });
 
         this.tutorial = this.add.sprite(game.config.width / 2, game.config.height, 'tutorial').setOrigin(0.5, 1)
-        this.tutorial.anims.play('tutorial');
+        this.tutorial.anims.play('tutorial');*/
     }
 }
